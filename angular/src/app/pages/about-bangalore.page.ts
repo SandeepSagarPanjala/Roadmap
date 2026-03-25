7d6a9d1bd8cdc96b9b47b6c42357f176bdaab224
Sandeep <sandeepsagar.panjala@gmail.com>
2026-03-25 01:47:32 -0500

    refactor: Remove About and Contact pages to streamline application structure

--- angular/src/app/pages/about-bangalore.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about-bangalore-page',
-  standalone: true,
-  templateUrl: './about-bangalore.page.html',
-})
-export class AboutBangalorePage {}
