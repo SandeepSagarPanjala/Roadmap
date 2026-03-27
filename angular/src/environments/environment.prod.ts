46dd39073cdf0a1dc5c1464bc411c492abfb6bc7
Sandeep <sandeepsagar.panjala@gmail.com>
2026-03-27 14:55:45 -0500

    feat: Add production environment configuration and enable file replacement for production builds.

---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  // When deploying, specify the absolute URL of the hosted backend server
+  apiUrl: 'http://localhost:3000/api', 
+  tokenStorageKey: 'nexus_access_token'
+};
