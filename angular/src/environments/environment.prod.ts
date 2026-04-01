c7c40dbf16ecd9155603e6885fd8e4e498d1cbd5
Sandeep <sandeepsagar.panjala@gmail.com>
2026-03-31 22:13:38 -0500

    fix: update GraphQL URL to use HTTPS for production environment

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  graphqlUrl: 'http://api.sandeepsagar.work/graphql',
+  graphqlUrl: 'https://api.sandeepsagar.work/graphql',
   tokenStorageKey: 'nexus_access_token',
 };
